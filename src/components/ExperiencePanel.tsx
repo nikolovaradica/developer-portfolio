f22a5c4d3e6cbb2cbfe8672b167568ddd3bcdeba
nikolovaradica <nikolova.radica@gmail.com>
2025-12-02 19:39:57 +0100

    change experience layout, page title and icon

--- src/components/ExperiencePanel.tsx
+++ src/components/ExperiencePanel.tsx
@@ -26,38 +26,53 @@ const ExperiencePanel = () => {
             initial={{ opacity: 0, y: 20 }}
             whileInView={{ opacity: 1, y: 0 }}
             viewport={{ once: true }}
-            className="relative bg-[#0D1117]/70 backdrop-blur-xl border border-cyan-400/20 rounded-xl p-6 md:p-8 mt-16 shadow-lg shadow-cyan-500/10 overflow-x-auto"
+            className="relative bg-[#0D1117]/70 backdrop-blur-xl border border-cyan-400/20 rounded-xl p-6 md:p-8 mt-16 shadow-lg shadow-cyan-500/10"
         >
             <div className="absolute top-0 left-0 w-full h-2 bg-cyan-400 rounded-t-xl"></div>
 
             <h3 className="text-xs text-cyan-300 font-mono tracking-widest mb-6">
-                <TypewriterText text="[ EXPERIENCE_PANEL ]"/>
+                <TypewriterText text="[ EXPERIENCE_PANEL ]" />
             </h3>
 
-            <div className="relative flex gap-16 min-w-max items-start">
-                <div className="absolute top-6 left-0 right-0 h-1 bg-gradient-to-r from-cyan-400 to-purple-500 z-0" />
+            <div className="flex relative">
+                <div className="lg:hidden relative flex flex-col items-center mr-7">
+                    <div
+                        className="w-[3px] bg-gradient-to-b from-cyan-400 to-purple-500 rounded-full absolute top-0 bottom-0 left-1/2 -translate-x-1/2"/>
 
-                {experiences.map((exp, idx) => (
-                    <div key={idx} className="relative flex flex-col items-center z-10 top-4 mb-4">
-                        <div className="w-5 h-5 bg-cyan-400 rounded-full shadow-[0_0_15px_#22d3ee] mb-6" />
+                    {experiences.map((_, idx) => (
+                        <div
+                            key={idx}
+                            className="w-5 h-5 bg-cyan-400 rounded-full shadow-[0_0_15px_#22d3ee] absolute left-1/2 -translate-x-1/2"
+                            style={{
+                                top: `calc(${(idx*1.15) * (100 / experiences.length)}% )`
+                            }}
+                        />
+                    ))}
+                </div>
 
-                        <motion.div
-                            whileHover={{ scale: 1.03, translateY: -4 }}
-                            transition={{ type: "spring", stiffness: 150 }}
-                            className="bg-[#0E1523]/60 border border-purple-400/30 rounded-xl p-6 shadow-md shadow-purple-500/10 w-72 flex-shrink-0"
-                        >
-                            <h3 className="mt-3 text-xl font-semibold text-cyan-300">
-                                {exp.role}
-                            </h3>
 
-                            <div className="mt-2 text-sm text-gray-300 opacity-80">
-                                {exp.years}
-                            </div>
+                <div
+                    className="hidden lg:block absolute top-2 left-0 right-0 h-1 bg-gradient-to-r from-cyan-400 to-purple-500 z-0 rounded-full"/>
 
-                            <p className="mt-3 text-gray-400 text-sm">{exp.description}</p>
-                        </motion.div>
-                    </div>
-                ))}
+                <div
+                    className="flex-1 flex flex-col lg:flex-row lg:gap-16 space-y-20 lg:space-y-0 lg:flex-nowrap overflow-x-auto">
+                    {experiences.map((exp, idx) => (
+                        <div key={idx} className="relative flex items-start lg:flex-col lg:items-center">
+                            <div
+                                className="absolute -left-10 top-0 w-5 h-5 bg-cyan-400 rounded-full shadow-[0_0_15px_#22d3ee] mb-6 lg:static lg:left-0 lg:top-auto"/>
+
+                            <motion.div
+                                whileHover={{scale: 1.03, translateY: -4}}
+                                transition={{type: "spring", stiffness: 150}}
+                                className="bg-[#0E1523]/60 border border-purple-400/30 rounded-xl p-6 shadow-md shadow-purple-500/10 w-full lg:w-64 lg:flex-1"
+                            >
+                                <h3 className="mt-3 text-xl font-semibold text-cyan-300">{exp.role}</h3>
+                                <div className="mt-2 text-sm text-gray-300 opacity-80">{exp.years}</div>
+                                <p className="mt-3 text-gray-400 text-sm">{exp.description}</p>
+                            </motion.div>
+                        </div>
+                    ))}
+                </div>
             </div>
         </motion.section>
     );
